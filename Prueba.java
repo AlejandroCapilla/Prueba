5dc3d97246fce89dc8a146b29e277c8da06e4f31
alejandrocapillasanchez <19031028@itcelaya.edu.mx>
2021-11-12 12:20:06 -0600

    Tercer commit

--- Prueba.java
+++ Prueba.java
@@ -2,6 +2,11 @@ package ProyectoPrueba;
 
 public class Prueba {
     public static void main(String[] args) {
-        
+        Prueba p = new Prueba();
+        p.imprimir();
+    }
+
+    private void imprimir() {
+        System.out.println("Hola mundo");
     }
 }
